41df3089289ecf1861ac8d9d115bd7dc9a731128
guillaumeogier <guiogier@sfr.fr>
2015-01-20 18:29:03 +0100

    debut

--- Memory/Form1.Designer.cs
+++ Memory/Form1.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace Memory
 {
-    partial class Form1
+    partial class memory
     {
         /// <summary>
-        /// Variable nécessaire au concepteur.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Nettoyage des ressources utilisées.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,203 @@
             base.Dispose(disposing);
         }
 
-        #region Code généré par le Concepteur Windows Form
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
-        /// le contenu de cette méthode avec l'éditeur de code.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(memory));
+            this.tlpTapisDeCartes = new System.Windows.Forms.TableLayoutPanel();
+            this.pb_01 = new System.Windows.Forms.PictureBox();
+            this.pb_02 = new System.Windows.Forms.PictureBox();
+            this.pb_03 = new System.Windows.Forms.PictureBox();
+            this.pb_04 = new System.Windows.Forms.PictureBox();
+            this.btn_Distribuer = new System.Windows.Forms.Button();
+            this.ilSabotDeCartes = new System.Windows.Forms.ImageList(this.components);
+            this.btn_Retourner = new System.Windows.Forms.Button();
+            this.btn_Jouer = new System.Windows.Forms.Button();
+            this.pb_Recherche = new System.Windows.Forms.PictureBox();
+            this.tlpTapisDeCartes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_04)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Recherche)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpTapisDeCartes
+            // 
+            this.tlpTapisDeCartes.ColumnCount = 4;
+            this.tlpTapisDeCartes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpTapisDeCartes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpTapisDeCartes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpTapisDeCartes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpTapisDeCartes.Controls.Add(this.pb_01, 0, 0);
+            this.tlpTapisDeCartes.Controls.Add(this.pb_02, 1, 0);
+            this.tlpTapisDeCartes.Controls.Add(this.pb_03, 2, 0);
+            this.tlpTapisDeCartes.Controls.Add(this.pb_04, 3, 0);
+            this.tlpTapisDeCartes.Location = new System.Drawing.Point(50, 25);
+            this.tlpTapisDeCartes.Name = "tlpTapisDeCartes";
+            this.tlpTapisDeCartes.RowCount = 1;
+            this.tlpTapisDeCartes.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpTapisDeCartes.Size = new System.Drawing.Size(440, 110);
+            this.tlpTapisDeCartes.TabIndex = 0;
+            // 
+            // pb_01
+            // 
+            this.pb_01.Location = new System.Drawing.Point(3, 3);
+            this.pb_01.Name = "pb_01";
+            this.pb_01.Size = new System.Drawing.Size(100, 104);
+            this.pb_01.TabIndex = 0;
+            this.pb_01.TabStop = false;
+            // 
+            // pb_02
+            // 
+            this.pb_02.Location = new System.Drawing.Point(113, 3);
+            this.pb_02.Name = "pb_02";
+            this.pb_02.Size = new System.Drawing.Size(100, 104);
+            this.pb_02.TabIndex = 0;
+            this.pb_02.TabStop = false;
+            // 
+            // pb_03
+            // 
+            this.pb_03.Location = new System.Drawing.Point(223, 3);
+            this.pb_03.Name = "pb_03";
+            this.pb_03.Size = new System.Drawing.Size(100, 104);
+            this.pb_03.TabIndex = 0;
+            this.pb_03.TabStop = false;
+            // 
+            // pb_04
+            // 
+            this.pb_04.Location = new System.Drawing.Point(333, 3);
+            this.pb_04.Name = "pb_04";
+            this.pb_04.Size = new System.Drawing.Size(100, 104);
+            this.pb_04.TabIndex = 0;
+            this.pb_04.TabStop = false;
+            // 
+            // btn_Distribuer
+            // 
+            this.btn_Distribuer.Location = new System.Drawing.Point(53, 307);
+            this.btn_Distribuer.Name = "btn_Distribuer";
+            this.btn_Distribuer.Size = new System.Drawing.Size(100, 23);
+            this.btn_Distribuer.TabIndex = 1;
+            this.btn_Distribuer.Text = "DISTRIBUER";
+            this.btn_Distribuer.UseVisualStyleBackColor = true;
+            this.btn_Distribuer.Click += new System.EventHandler(this.btn_Distribuer_Click);
+            // 
+            // ilSabotDeCartes
+            // 
+            this.ilSabotDeCartes.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilSabotDeCartes.ImageStream")));
+            this.ilSabotDeCartes.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilSabotDeCartes.Images.SetKeyName(0, "DosCarte.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(1, "Dialogue.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(2, "Diplome.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(3, "FemmeAffaire.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(4, "FemmeBatiment.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(5, "FemmeBlanche.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(6, "FemmeBleue.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(7, "FemmeCasque.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(8, "FemmeCuisinier.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(9, "FemmeInformaticienBrune.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(10, "FemmeInformaticienChatain.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(11, "FemmeJournal.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(12, "FemmeMedecin.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(13, "FemmeMegaphone.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(14, "FemmeOrange.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(15, "FemmePhotographe.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(16, "FemmePoliciere.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(17, "HommeAffaire.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(18, "HommeBatiment.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(19, "HommeBleu.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(20, "HommeCasque.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(21, "HommeCuisinier.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(22, "HommeGarde.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(23, "HommeInformaticienBrun.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(24, "HommeInformaticienChatain.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(25, "HommeJournal.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(26, "HommeMarron.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(27, "HommeMedecin.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(28, "HommeMegaphone.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(29, "HommeOrange.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(30, "HommePhotographe.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(31, "HommePolicier.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(32, "Joker.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(33, "LivreurPizza.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(34, "MarinMousse.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(35, "MarinOfficier.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(36, "MarinPompon.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(37, "Mecanicien.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(38, "Pilote.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(39, "Pompier.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(40, "Serveur.jpg");
+            // 
+            // btn_Retourner
+            // 
+            this.btn_Retourner.Location = new System.Drawing.Point(163, 307);
+            this.btn_Retourner.Name = "btn_Retourner";
+            this.btn_Retourner.Size = new System.Drawing.Size(100, 23);
+            this.btn_Retourner.TabIndex = 2;
+            this.btn_Retourner.Text = "RETOURNER";
+            this.btn_Retourner.UseVisualStyleBackColor = true;
+            this.btn_Retourner.Click += new System.EventHandler(this.btn_Retourner_Click);
+            // 
+            // btn_Jouer
+            // 
+            this.btn_Jouer.Location = new System.Drawing.Point(273, 307);
+            this.btn_Jouer.Name = "btn_Jouer";
+            this.btn_Jouer.Size = new System.Drawing.Size(100, 23);
+            this.btn_Jouer.TabIndex = 2;
+            this.btn_Jouer.Text = "JOUER";
+            this.btn_Jouer.UseVisualStyleBackColor = true;
+            this.btn_Jouer.Click += new System.EventHandler(this.btn_Jouer_Click);
+            // 
+            // pb_Recherche
+            // 
+            this.pb_Recherche.Location = new System.Drawing.Point(383, 226);
+            this.pb_Recherche.Name = "pb_Recherche";
+            this.pb_Recherche.Size = new System.Drawing.Size(100, 104);
+            this.pb_Recherche.TabIndex = 0;
+            this.pb_Recherche.TabStop = false;
+            // 
+            // memory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(535, 359);
+            this.Controls.Add(this.btn_Jouer);
+            this.Controls.Add(this.btn_Retourner);
+            this.Controls.Add(this.btn_Distribuer);
+            this.Controls.Add(this.pb_Recherche);
+            this.Controls.Add(this.tlpTapisDeCartes);
+            this.Name = "memory";
+            this.Text = "le jeu du memory";
+            this.Load += new System.EventHandler(this.memory_Load);
+            this.tlpTapisDeCartes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_04)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Recherche)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpTapisDeCartes;
+        private System.Windows.Forms.PictureBox pb_01;
+        private System.Windows.Forms.PictureBox pb_02;
+        private System.Windows.Forms.PictureBox pb_03;
+        private System.Windows.Forms.PictureBox pb_04;
+        private System.Windows.Forms.Button btn_Distribuer;
+        private System.Windows.Forms.ImageList ilSabotDeCartes;
+        private System.Windows.Forms.Button btn_Retourner;
+        private System.Windows.Forms.Button btn_Jouer;
+        private System.Windows.Forms.PictureBox pb_Recherche;
+
     }
 }
-
--- Memory/Form1.cs
+++ Memory/Form1.cs
@@ -7,14 +7,138 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dllLoto;
 
 namespace Memory
 {
-    public partial class Form1 : Form
+    public partial class memory : Form
     {
-        public Form1()
+        // Déclaration des variables globales du jeu
+        int nbCartesDansSabot;  // Nombre de cartes dans le sabot (en fait nombre
+        // d'images dans le réservoir)
+        int nbCartesSurTapis;   // Nombre de cartes sur le tapis
+        int[] cartesTiree;
+
+        private void Distribution_Aleatoire()
+        {
+            cartesTiree = new int[nbCartesSurTapis];
+            // On utilise la LotoMachine pour générer une série aléatoire
+            LotoMachine hasard = new LotoMachine(nbCartesDansSabot);
+            // On veut une série de nbCartesSurTapis cartes parmi celles 
+            // du réservoir
+            int[] tImagesCartes = hasard.TirageAleatoire(nbCartesSurTapis, false);
+            // La série d'entiers retournée par la LotoMachine correspondra
+            // aux indices des cartes dans le "sabot"
+
+            // Affectation des images aux picturebox
+            PictureBox carte;
+            int i_image;
+            for (int i_carte = 0; i_carte < nbCartesSurTapis; i_carte++)
+            {
+                carte = (PictureBox)tlpTapisDeCartes.Controls[i_carte];
+                i_image = tImagesCartes[i_carte + 1]; // i_carte + 1 à cause
+                // des pbs d'indices
+                cartesTiree[i_carte] = i_image;
+                carte.Image = ilSabotDeCartes.Images[i_image];
+            }
+        }
+
+        private void Retournement()
+        {
+            PictureBox carte;
+            foreach (Control ctrl in tlpTapisDeCartes.Controls)
+            {
+                // Je sais que le contrôle est une PictureBox
+                // donc je "caste" l'objet (le Contrôle) en PictureBox...
+                carte = (PictureBox)ctrl;
+                // Ensuite je peux accéder à la propriété Image
+                // (je ne pourrais pas si je n'avais pas "casté" le contrôle)
+                carte.Image = ilSabotDeCartes.Images[0];
+            }
+        }
+
+       
+        
+
+        public memory()
         {
             InitializeComponent();
+        }
+
+        private void Distribution_Sequentielle()
+        {
+            PictureBox carte;
+            int i_carte = 1;
+
+            foreach (Control ctrl in tlpTapisDeCartes.Controls)
+            {
+                // Je sais que le contrôle est une PictureBox
+                // donc je "caste" l'objet (le Contrôle) en PictureBox...
+                carte = (PictureBox)ctrl;
+                // Ensuite je peux accéder à la propriété Image
+                // (je ne pourrais pas si je n'avais pas "casté" le contrôle)
+                carte.Image = ilSabotDeCartes.Images[i_carte];
+                i_carte++;
+            }
+        }
+
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_Distribuer_Click(object sender, EventArgs e)
+        {
+            // On récupère le nombre d'images dans le réservoir :
+            nbCartesDansSabot = ilSabotDeCartes.Images.Count - 1;
+            // On enlève 1 car :
+            // -> la l'image 0 ne compte pas c’est l’image du dos de carte 
+            // -> les indices vont de 0 à N-1, donc les indices vont jusqu’à 39
+            //    s’il y a 40 images au total *
+
+            // On récupère également le nombre de cartes à distribuées sur la tapis
+            // autrement dit le nombre de contrôles présents sur le conteneur
+            nbCartesSurTapis = tlpTapisDeCartes.Controls.Count;
+
+            // On effectue la distribution (aléatoire) proprement dite
+            Distribution_Aleatoire();
+        }
+
+        private void memory_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Retourner_Click(object sender, EventArgs e)
+        {
+            Retournement();
+        }
+
+        private void btn_Jouer_Click(object sender, EventArgs e)
+        {
+            LotoMachine hasard = new LotoMachine(nbCartesSurTapis);
+            int[] tImagesCartes = hasard.TirageAleatoire(nbCartesSurTapis, false);
+            int i = hasard.NumeroAleatoire();
+            switch(i)
+            {
+                case 1 : 
+                    pb_Recherche.Image = ilSabotDeCartes.Images[cartesTiree[0]];
+                    break;
+                case 2 :
+                    pb_Recherche.Image = ilSabotDeCartes.Images[cartesTiree[1]];
+                    break;
+                case 3 :
+                    pb_Recherche.Image = ilSabotDeCartes.Images[cartesTiree[2]];
+                    break;
+                case 4 :
+                    pb_Recherche.Image = ilSabotDeCartes.Images[cartesTiree[3]];
+                    break;
+            }
+             
+
+            Retournement();
+          
         }
     }
 }
